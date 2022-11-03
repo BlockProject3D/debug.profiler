5b21b4c1e0a3db9b1d12ec86e6c0bee395023328
Yuri6037 <yuri6037@outlook.com>
2022-11-03 15:49:33 +0100

    Added session module

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -1,10 +1,10 @@
 // Copyright (c) 2022, BlockProject 3D
-//
+// 
 // All rights reserved.
-//
+// 
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
-//
+// 
 //     * Redistributions of source code must retain the above copyright notice,
 //       this list of conditions and the following disclaimer.
 //     * Redistributions in binary form must reproduce the above copyright notice,
@@ -13,7 +13,7 @@
 //     * Neither the name of BlockProject 3D nor the names of its contributors
 //       may be used to endorse or promote products derived from this software
 //       without specific prior written permission.
-//
+// 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -26,31 +26,12 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-use server::Server;
-use tokio::io::AsyncReadExt;
+pub struct Session {
 
-mod session;
-mod network_types;
-mod server;
-mod client;
-mod client_manager;
-
-async fn run() {
-    let server = Server::new("127.0.0.1:25565").await;
-    match server {
-        Ok(v) => {
-            let mut stdin = tokio::io::stdin();
-            let mut buffer: [u8; 256] = [0; 256];
-            let flag = stdin.read(&mut buffer).await.map(|v| v == 0).unwrap_or(true);
-            if flag {
-                v.stop().await;
-            }
-        },
-        Err(e) => eprintln!("Failed to start server: {}", e)
-    }
 }
 
-#[tokio::main]
-async fn main() {
-    run().await
+impl Session {
+    pub fn new() -> Session {
+        Session { }
+    }
 }
